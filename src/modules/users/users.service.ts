4740f2101ca5fc82bab48b7c6fd7ea1caa7b6ba3
Jefferson Oliveira <oliveira2112jj@outlook.com>
2025-10-21 22:24:06 -0300

    config prisma service, generate users resource and create user

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prismaService: PrismaService) {}
+  create(createUserDto: CreateUserDto) {
+    return this.prismaService.user.create({
+      data: createUserDto,
+    });
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: string) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    console.log({ updateUserDto });
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: string) {
+    return `This action removes a #${id} user`;
+  }
+}
