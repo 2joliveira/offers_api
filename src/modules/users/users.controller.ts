5414b1689958b78678b437a1bf7ceaaffbabaadb
Jefferson Oliveira <oliveira2112jj@outlook.com>
2025-10-22 21:36:59 -0300

    create validation pipe with zod and PipeTransform

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -6,16 +6,33 @@ import {
   Patch,
   Param,
   Delete,
+  HttpCode,
+  UsePipes,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import z from 'zod';
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe';
+
+const createUserDto = z.object({
+  name: z.string(),
+  userName: z.string(),
+  email: z.email(),
+  password: z.string(),
+});
+
+const updateUserDto = createUserDto.partial();
+
+export type CreateUserDto = z.infer<typeof createUserDto>;
+
+export type UpdateUserDto = z.infer<typeof updateUserDto>;
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createUserDto))
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
@@ -31,6 +48,7 @@ export class UsersController {
   }
 
   @Patch(':id')
+  @UsePipes(new ZodValidationPipe(updateUserDto))
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
